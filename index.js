de63485b870567f3aa3ecfe7a4cdd14674137163
incha111 <87800456+incha111@users.noreply.github.com>
2022-09-11 10:20:25 +0200

    Feature b (#3)
    * added header to index.js
    
    * added one paragraph to index.js
    
    * added another paragraph to index.js

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 Console.log("Hello git");
 Console.log("Rebase");
+Console.log("Header");
 Console.log("First paragraph");
+Console.log("Paragraph");
+Console.log("Another paragraph");
 Console.log("Last paragraph");
